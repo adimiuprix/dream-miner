b65cdc044341202d314b7ddf8bbc6b7594989745
adimiuprix <miuprix@gmail.com>
2026-06-07 22:00:17 +0700

    feat(prisma): remove price field from Contract model
    - Drop `price` column from Contract table via migration
    - Remove `price` field from Contract schema definition
    - Simplify contract data model by removing pricing information stored at contract level

---
+++ prisma/migrations/20260607145914_remove_contract_price/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `price` on the `Contract` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Contract" DROP COLUMN "price";
